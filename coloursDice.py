fbebd83cc206787ef225f04f1596c853c47e235c
Marcel <marcel.wedi@ds.mpg.de>
2019-06-03 14:34:21 +0200

    Replaced numbers with colours

--- coloursDice.py
+++ coloursDice.py
@@ -17,8 +17,13 @@ def write_log_file(outputfilename, data):
     f.write("My randomly generated number is " + str(data) + " (" + time.strftime("%H:%M:%S") + ")\n")
     f.close()
 
+def get_colour_by_dice(spots):
+    colordict = {1: 'red', 2: 'blue', 3: 'purple', 4: 'yellow', 5: 'green', 6: 'black'}
+    return colordict[spots]
 
 if __name__ == "__main__":
     outputfilename = "randomNumber"
-    roll = get_random_number(1, 100)
-    write_log_file(outputfilename, roll)
+    roll = get_random_number(1, 6)
+    color = get_colour_by_dice(roll)
+    write_log_file(outputfilename, color)
+
