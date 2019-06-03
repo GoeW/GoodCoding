9ffce92a56a3af13814f01f280df233a501458f9
Marcel <marcel.wedi@ds.mpg.de>
2019-06-03 16:21:59 +0200

    added array of six values

--- randomNumber.py
+++ randomNumber.py
@@ -24,7 +24,12 @@ def get_colour_by_dice(spots):
 
 if __name__ == "__main__":
     outputfilename = "randomNumber"
-    roll = get_random_number(1, 6)
+    rolls = []
+    for i in range(6):
+        roll = get_random_number(1, 6)
+        rolls.append(roll)
     color = get_colour_by_dice(roll)
     write_log_file(outputfilename, color)
+    print(rolls)
+    sys.stdout.flush()
 
